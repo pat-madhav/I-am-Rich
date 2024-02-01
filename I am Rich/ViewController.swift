f2cf31015e0cea43b3ce563aab15ee43d0d49907
Pat Madhavaram <pattabhi.madhav@gmail.com>
2024-01-31 20:44:52 -0600

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Pattabhi Madhavaram on 1/31/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
